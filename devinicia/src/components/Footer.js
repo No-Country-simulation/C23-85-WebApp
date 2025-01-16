f3d270e611ce11eda08fa3d3f17cf8663cf2aea2
Sebastian Siddi <ssiddi@gmail.com>
2025-01-16 16:31:11 -0300

    Actualización de estilos

--- devinicia/src/components/Footer.js
+++ devinicia/src/components/Footer.js
@@ -4,7 +4,8 @@ import '../styles/styles.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Programación Ilimitada. Todos los derechos reservados.</p>
+      <p>&copy; 2025 Programación Ilimitada. Todos los derechos reservados. No-Country C23-85-WebApp
+      </p>
       <a href="#contact" className="footer-link">Contáctenos</a>
     </footer>
   );
